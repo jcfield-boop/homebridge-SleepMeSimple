e0cf1958821059500a2b38edb3bc779b59348d34
James Field <56139576+jcfield-boop@users.noreply.github.com>
2025-03-23 14:13:53 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,15 @@
  */
 import { API } from 'homebridge';
 import { SleepMeSimplePlatform } from './platform.js';
-import { PLATFORM_NAME } from './settings.js';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 /**
  * This function is called by Homebridge to register the platform with Homebridge
  */
-export default (api: API): void => {
+export = (api: API) => {
   // Register the platform
   api.registerPlatform(PLATFORM_NAME, SleepMeSimplePlatform);
   
   // Log plugin initialization
-  const log = api.logger;
-  log.info(`SleepMe Simple platform registered`);
+  api.log.info(`${PLUGIN_NAME} plugin initialized`);
 };
