32a7ed69cdfbe607c4f709bfdc1f7c13bc58ca06
James Field <56139576+jcfield-boop@users.noreply.github.com>
2025-03-25 20:43:04 -0700

    Create index.js

---
+++ customui/index.js
@@ -0,0 +1,7 @@
+// This file serves as an entry point for our custom UI components
+// Homebridge will automatically load this file when the plugin is loaded
+
+// Load our template-selector handler
+import './template-selector-handler.js';
+
+console.log('SleepMe Simple Custom UI loaded');
